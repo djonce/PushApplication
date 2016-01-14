3cb78717aee8b2b5557fe0dfefcb0a24c68b8dfd
djonce <891879872@qq.com>
2016-01-14 14:58:48 +0800

    commit readme docs

--- sample/src/main/java/com/qunduikj/pushapplication/PushReceiver.java
+++ sample/src/main/java/com/qunduikj/pushapplication/PushReceiver.java
@@ -3,14 +3,32 @@ package com.qunduikj.pushapplication;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.igexin.sdk.PushConsts;
 
 /**
  * Created by wangj on 2016/1/13
  */
 public class PushReceiver extends BroadcastReceiver{
 
+    private static final String TAG = PushReceiver.class.getSimpleName();
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        Bundle bundle = intent.getExtras();
+
+        switch (bundle.getInt(PushConsts.CMD_ACTION)){
+            case PushConsts.GET_MSG_DATA:  // 自定义透传消息
+                byte[] payload = bundle.getByteArray("payload");
+
+                if(payload != null) {
+                    String data = new String(payload);
+                    Log.e(TAG, "----- data :" + data);
+                }
+                break;
+        }
     }
 }
